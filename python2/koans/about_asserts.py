99b7f770991ca1f29cbb2b17af87d5d1d599bbbc
jordnV <jord.vanessa@gmail.com>
2015-01-28 21:24:54 +0000

    completed 7 koans

--- python2/koans/about_asserts.py
+++ python2/koans/about_asserts.py
@@ -11,7 +11,8 @@ class AboutAsserts(Koan):
         We shall contemplate truth by testing reality, via asserts.
         """
 
-        # Confused? This video should help:
+        # Confused? This vi
+        #deo should help:
         #
         #   http://bit.ly/about_asserts
 
@@ -27,14 +28,14 @@ class AboutAsserts(Koan):
         """
         Sometimes we will ask you to fill in the values
         """
-        self.assertEqual(__, 1 + 1)
+        self.assertEqual(2, 1 + 1)
 
     def test_assert_equality(self):
         """
         To understand reality, we must compare our expectations against
         reality.
         """
-        expected_value = __
+        expected_value = 2
         actual_value = 1 + 1
         self.assertTrue(expected_value == actual_value)
 
@@ -42,7 +43,7 @@ class AboutAsserts(Koan):
         """
         Some ways of asserting equality are better than others.
         """
-        expected_value = __
+        expected_value = 2
         actual_value = 1 + 1
 
         self.assertEqual(expected_value, actual_value)
@@ -53,7 +54,7 @@ class AboutAsserts(Koan):
         """
 
         # This throws an AssertionError exception
-        assert False
+        assert True
 
     def test_that_sometimes_we_need_to_know_the_class_type(self):
         """
@@ -72,7 +73,7 @@ class AboutAsserts(Koan):
         #
         # See for yourself:
 
-        self.assertEqual(__, "naval".__class__) # It's str, not <type 'str'>
+        self.assertEqual(str, "naval".__class__) # It's str, not <type 'str'>
 
         # Need an illustration? More reading can be found here:
         #
